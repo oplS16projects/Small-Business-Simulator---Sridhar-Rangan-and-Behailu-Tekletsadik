755c542ac1fe1e21d89b135d70ccac449dfb77e0
SridharRangan <Sridhar_Rangan@student.uml.edu>
2016-04-27 20:28:32 -0400

    Added Working HTML Code

--- HTMLRangan.rkt
+++ HTMLRangan.rkt
@@ -1,13 +1,53 @@
 #lang web-server/insta
  
-(struct post (title body))
+(struct post (title body third fourth fifth))
  
 (define BLOG
-  (list (post "Filler code" "filler")))
+  (list (post " " " " " " " " " ")))
  
 (define (start request)
-  (render-blog-page BLOG request))
+  (local [(define a-blog
+            (cond [(can-parse-post? (request-bindings request))
+                   (cons (parse-post (request-bindings request))
+                         BLOG)]
+                  [else
+                   BLOG]))]
+         (render-blog-page a-blog request)))
 
+(define (can-parse-post? bindings)
+  (and (exists-binding? 'title bindings)
+       (exists-binding? 'body bindings)
+       (exists-binding? 'third bindings)
+       (exists-binding? 'fourth bindings)
+       (exists-binding? 'fifth bindings)))
+
+(define strike 0)
+(define stock 0)
+(define time 0)
+(define interest 0)
+(define volatility 0)
+
+(define (parse-post bindings)
+  (set! strike (string->number (extract-binding/single 'title bindings)))
+  (set! stock (string->number (extract-binding/single 'body bindings)))
+  (set! time (string->number (extract-binding/single 'third bindings)))
+  (set! interest (string->number (extract-binding/single 'fourth bindings)))
+  (set! volatility (string->number (extract-binding/single 'fifth bindings)))
+  (display "Strike: ")
+  (display strike)
+  (display "\nStock: ")
+  (display stock)
+  (display "\nTime: ")
+  (display time)
+  (display "\nInterest: ")
+  (display interest)
+  (display "\nVolatility: ")
+  (display volatility)
+  (post (extract-binding/single 'title bindings)
+        (extract-binding/single 'body bindings)
+        (extract-binding/single 'third bindings)
+        (extract-binding/single 'fourth bindings)
+        (extract-binding/single 'fifth bindings)))
 
 (define (render-blog-page a-blog request)   
   (response/xexpr
@@ -19,13 +59,20 @@ This program uses modeling such as the monte carlo method and black-scholes mode
 
 (h1 "Instructions")
 (p "Below you will need to input different values for our different models. Please input them as requested below.")
-(h1 "Values For Modeling")
+(h1 "Values For Black-Scholes")
+               ,(render-posts a-blog)
+               (form
                 "Input Strike Price: "
-                (input ((type "text")(number "StrikePrice")))
-                "Input Stock Price: "
-                (input ((type "text")(number "StockPrice")))
-                (input ((type "submit")(value "Submit")))))))
-
+                (input ((name "title")))
+                "\nInput Stock Price: "
+                (input ((name "body")))
+                "\nInput time: "
+                (input ((name "third"))
+                "\nInput interest: "
+                (input ((name "fourth"))
+                "\nInput volatility: "
+                (input ((name "fifth"))
+                (input ((type "submit")))))))))))
 
 
 (define (render-post a-post)
